fb2936b5a1ed6c42fe362765f05e645bfc27a17c
Qiming <LuQiMing@aactechnologies.com>
2024-04-10 17:28:21 +0800

    Added Lua 5.2 support
    We build Lua 52. source codes by ourselves.

--- README.md
+++ README.md
@@ -1,13 +1,2 @@
-# Пример библиотеки на C++ для Lua
-Адаптировано для использования в торговом терминале QUIK.
-Проект для Visual Studio содержит готовые варианты сборки для разных версий QUIK.
-
-Подробное описание:
-https://quik2dde.ru/viewtopic.php?id=18
-
-## Варианты сборки
-
-- Release / Debug | Win32 - для QUIK 6.x / 7.x (x32, Lua5.1)
-- Release / Debug | x64 - для QUIK 8.0...8.4 (x64, Lua5.1)
-- Release-Lua53 / Debug-Lua53 | x64 - для QUIK 8.5 и далее (x64, Lua5.3)
-- Release-Lua54 / Debug-Lua54 | x64 - для QUIK 8.11 и далее (x64, Lua5.4)
+A simple demo about how to create a Lua-ready DLL.
+We export several APIs for Lua to invoke. You can see testing lua script codes in the project: lua_windows_build, where we build lua v5.2.0 source codes using Visual Studio 2019 on Windows.
--- src/luacdll.cpp
+++ src/luacdll.cpp
@@ -1,22 +1,17 @@
 //
-// Пример простой библиотеки на C++ для вызова ее из LUA
+// How to create a Lua-ready DLL
 // http://quik2dde.ru/viewtopic.php?id=18
 //
 
 #include <windows.h>
 #include <process.h>
+#include <tchar.h>
 
-// в случае вызова функций из Lua-кода во внешней DLL
-// необходимо определить эти константы до подключения заголовочных файлов Lua
 #define LUA_LIB
 #define LUA_BUILD_AS_DLL
 
-// подключаем заголовочные файлы из дистрибутива Lua
-// т.к. наш проект C++, то подключаем единый файл заголовка, где правильно подключаются заголовочные файлы Lua, сделанные для "чистого C"
-// правильный путь к файлам Lua5.1, Lua5.3 или Lua5.4 задан в настройках проекта (разный в зависимости от выбранного варианта в Solution Configurations)
 #include "lua.hpp"
 
-// стандартная точка входа для DLL
 BOOL APIENTRY DllMain( HANDLE hModule, 
                        DWORD  ul_reason_for_call, 
                        LPVOID lpReserved
@@ -25,27 +20,26 @@ BOOL APIENTRY DllMain( HANDLE hModule,
 	return TRUE;
 }
 
-// реализация пользовательских функций, вызываемых из Lua
 
 static int forLua_GetCurrentThreadId(lua_State *L) {
-	// возвращаем одно целочисленное значение, полученное от Win API функции
     lua_pushinteger(L, GetCurrentThreadId());
 	return(1);
 }
 
 static int forLua_MultTwoNumbers(lua_State *L) {
-	// получаем первый и второй параметры вызова функции из стека с проверкой каждого на число
     double d1 = luaL_checknumber(L, 1);
     double d2 = luaL_checknumber(L, 2);
 
-	// помещаем в стек результат умножения
     lua_pushnumber(L, d1 * d2);
 
-    return(1);  // эта функция возвращает одно значение
+	// Just for test
+	MessageBox(NULL, _T("forLua_MultTwoNumbers invoked."), _T("QM"), MB_OK);
+
+    return(1); 
 }
 	
 static int forLua_MultAllNumbers(lua_State *L) {
-	const int n = lua_gettop(L);  // количество переданных аргументов
+	const int n = lua_gettop(L); 
 	double res = 1;
 	bool isNumberFound = false;
 	for (int i = 1; i <= n; ++i)
@@ -63,7 +57,7 @@ static int forLua_MultAllNumbers(lua_State *L) {
     return(1);
 }
 	
-// список реализованных в dll пользовательских функций
+// DLL APIs for Lua
 static struct luaL_Reg ls_lib[] = {
     {"GetCurrentThreadId", forLua_GetCurrentThreadId},
     {"MultTwoNumbers", forLua_MultTwoNumbers},
@@ -71,10 +65,8 @@ static struct luaL_Reg ls_lib[] = {
     {nullptr, nullptr}
 };
 
+// Entry point for Lua-ready DLL
 extern "C" LUALIB_API int luaopen_luacdll(lua_State *L) {
-	// эта функция выполнится в момент вызова require() из Lua-кода
-	// здесь регистрируем доступные из dll функции, тогда они станут доступны нам из Lua
-	// в Lua 5.1 и Lua 5.3 для регистрации предназначены разные функции
 	#if LUA_VERSION_NUM >= 502
 		luaL_newlib(L, ls_lib);
 	#else
